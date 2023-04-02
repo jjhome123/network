d531a2ced9191d84d96b903917a0558839b01c46
jjhome123 <johnplyee@gmail.com>
2023-04-02 15:20:00 +0900

    updates following count

--- network/static/network/posts.js
+++ network/static/network/posts.js
@@ -1,15 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Adds edit functionality to edit buttons on profile or index page
     edit = document.querySelectorAll('.edit');
     edit.forEach(edit => {
         edit.addEventListener('click', () => {
             edit_entry(edit.dataset.post_id)
         })
     });
+
+    // Adds follow functionality to follow button on profile page
+    if (document.querySelector('h3').innerHTML !== 'Your Profile') {
+        user = document.querySelector('h5').innerHTML;
+        follow_button = document.querySelector('#follow');
+        if (follow_button !== null) {
+            follow_button.onclick = () => {
+                if (follow_button.innerHTML.includes(' Follow')) {
+                    fetch(`/profile/${user}`, {
+                        method: "POST",
+                        body: JSON.stringify({
+                            follow: true
+                        })
+                    })
+                    .then(document.querySelector('#follower-num').innerHTML = parseInt(document.querySelector('#follower-num').innerHTML) + 1);
+                    follow_button.innerHTML = 'Un-Follow';
+                } else if (follow_button.innerHTML.includes('Un-Follow')) {
+                    fetch(`/profile/${user}`, {
+                        method: "POST",
+                        body: JSON.stringify({
+                            follow: false
+                        })   
+                    })
+                    .then(document.querySelector('#follower-num').innerHTML -= 1);
+                    follow_button.innerHTML = ' Follow';
+                }
+            }
+        }
+    }
 });
 
 function edit_entry(post_id) {
     field = document.querySelector(`#edit_field${post_id}`);
     post_body = document.querySelector(`#post_body${post_id}`)
     field.style.display = 'block';
+    field.focus();
     post_body.style.display = 'none';
 }
--- network/views.py
+++ network/views.py
@@ -3,41 +3,64 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 from .models import User, Post, Like, Follow
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('datetime')
     if request.method == "POST":
         if request.POST.get("post_contents"):
             p = Post(pk=request.POST.get("post_id"), poster=request.user)
             p.post = request.POST.get("post_contents")
-            p.save()
-            
+            p.save()   
         elif not request.POST.get("new_post"):
             return render(request, "network/index.html",{
                 "posts": posts,
                 "message": "Error: Blank posts are not allowed."
             })
         else:
-            p = Post(poster=request.user, post=request.POST["post"])
-            p.save()
+            p = Post(poster=request.user, post=request.POST.get("new_post"))
+            p.save()    
     return render(request, "network/index.html", {
         "posts": posts
     })
 
+@csrf_exempt
 def profile(request, user):
     if request.method == 'POST':
         if request.POST.get("post_contents"):
             p = Post(pk=request.POST.get("post_id"), poster=request.user)
             p.post = request.POST.get("post_contents")
             p.save()
+        elif request.body:
+            follow = json.loads(request.body)
+            print(follow["follow"])
+            if follow["follow"]:
+                print('Following')
+                f = Follow(user=request.user, following_user=User.objects.get(username=user))
+                f.save()
+            elif not follow["follow"]:
+                print('Unfollowing')
+                f = Follow.objects.get(user=request.user, following_user=User.objects.get(username=user))
+                f.delete()
     profile_user = User.objects.get(username=user)
-    posts = Post.objects.filter(poster=profile_user)
+    posts = Post.objects.filter(poster=profile_user).order_by('datetime')
+    followers = Follow.objects.filter(following_user=profile_user).count()
+    following = Follow.objects.filter(user=profile_user).count()
+    print(following)
+    try:
+        Follow.objects.get(user=request.user, following_user=User.objects.get(username=user))
+        f_status = True
+    except:
+        f_status = False
     return render(request, "network/profile.html",{
         "posts": posts,
-        "user": profile_user.username
+        "user": profile_user.username,
+        "followers": followers,
+        "following": following,
+        "f_status": f_status
     })
 
 
